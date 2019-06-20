485085be39e902a66426613d0380bebabdecfb1c
Rishu Garg <rishugarg102@gmail.com>
2019-06-20 13:51:19 +0530

    celebrityListAdapter
    created class which inherits from BaseAdapter class and is abstarct class so we need to implements method of abstarct class

--- app/src/main/java/com/example/celebrities/MainActivity.kt
+++ app/src/main/java/com/example/celebrities/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.celebrities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private  var celebrityAdapter:celebrityListAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        celebrityAdapter = celebrityListAdapter(this@MainActivity)
+        celebrity_ListView.adapter = celebrityAdapter
     }
 }
---
+++ app/src/main/java/com/example/celebrities/celebrityListAdapter.kt
@@ -0,0 +1,56 @@
+package com.example.celebrities
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.celebrity_row.view.*
+
+class celebrityListAdapter:BaseAdapter {
+    private var context:Context? = null
+    constructor(context:Context)
+    {
+          celebrityDatabase = celebrity_database()
+    }
+    private var celebrityDatabase:celebrity_database? = null
+    constructor()
+    {
+        celebrityDatabase = celebrity_database()
+        this.context = context
+    }
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val celebrity:celebrity = celebrityDatabase?.celebrityList?.get(position)
+            ?: celebrity("No Actor","No Description",R.drawable.placeholder,false)
+
+        var celebrityView:View
+        var layoutInflator:LayoutInflater = context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        celebrityView = layoutInflator.inflate(R.layout.celebrity_row,null)
+
+        celebrityView.actor_image1.setImageResource(celebrity?.image ?:R.drawable.placeholder)
+        celebrityView.lbl_ActorName.setText(celebrity.name)
+        celebrityView.lbl_ActorDes.setText(celebrity.des)
+
+        return celebrityView
+    }
+
+    override fun getItem(position: Int): Any {                                                //celebrity objects according to position
+            return  celebrityDatabase?.celebrityList?.get(position) ?:
+            celebrity("No Actor ","No Description",R.drawable.placeholder,false)
+    }
+
+    override fun getItemId(position: Int): Long {                                             //access index value of arrayList
+
+        return position.toLong()
+
+    }
+
+    override fun getCount(): Int {
+        return celebrityDatabase?.celebrityList?.size ?:0                                       //?: elvis operator
+                                                                                                 // because our list is nullable but return type is not nullable we used elvis operator
+                                                                                                // specify default value
+    }
+}
